3ebbc3173dca039dede69a44dbc1d87b46278393
gkond <gkondratenko@gmail.com>
2021-07-14 15:07:27 +0300

    Cleanup code

--- src/components/common/View/index.tsx
+++ src/components/common/View/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./styles.css";
 
-export const View:React.FunctionComponent<{ title: string, content: React.ReactElement }> = ({title, content}) => {
+export const View:React.FunctionComponent<{
+  title: string,
+  content: React.ReactElement
+}> = ({title, content}) => {
   return (
     <div className="view">
       <h1 className="view_title">
